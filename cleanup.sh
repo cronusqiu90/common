86a5870a36b3e2fdae33afde5fb85748133232d1
cronusqiu90 <cronusqiu90@gmail.com>
2024-06-19 12:12:23 +0800

    only echo trunc log

--- cleanup.sh
+++ cleanup.sh
@@ -14,16 +14,16 @@ rm -rf $target/*2024-05*.log
 find $target -type f -name "*.log" -print0 | while IFS= read -r -d $'\0' filePath; do
     fileName=$(basename "$filePath")
     if [[ $fileName =~ ^[1-9] ]];then
-        echo "[-]    $filePath"
+        # echo "[-]    $filePath"
         continue
     fi
 
-    echo "[*] $filePath"
+    # echo "[*] $filePath"
 
     fsize=$(stat -c "%s" "$filePath")
     fsizeMB=$(echo "scale=2; $fsize / 1024 / 1024" | bc)
     if (( $(awk -v size="$fsizeMB" 'BEGIN {print (size > 20 ? 1 : 0)}') )); then
-        echo "[+] $filePath   [$fsizeMB] "
+        echo "[TRUNC] $filePath   [$fsizeMB] "
         truncate -s 0 $filePath
     fi
 done

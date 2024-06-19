86e62a1813acd61478e0952d0f8097b50df220d7
cronusqiu90 <cronusqiu90@gmail.com>
2024-06-19 12:05:53 +0800

    update

---
+++ cleanup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+target=$1
+
+rm -rf $target/*2021*.log
+rm -rf $target/*2022*.log
+rm -rf $target/*2023*.log
+rm -rf $target/*2024-01*.log
+rm -rf $target/*2024-02*.log
+rm -rf $target/*2024-03*.log
+rm -rf $target/*2024-04*.log
+rm -rf $target/*2024-05*.log
+
+find $target -type f -name "*.log" -print0 | while IFS= read -r -d $'\0' filePath; do
+    fileName=$(basename "$filePath")
+    if [[ $fileName =~ ^[1-9] ]];then
+        # echo "SKIP $filePath"
+        continue
+    fi
+
+    fsize=$(stat -c "%s" "$filePath")
+    fsizeMB=$(echo "scale=2; $fsize / 1024 / 1024" | bc)
+    if (( $(awk -v size="$fsizeMB" 'BEGIN {print (size > 20 ? 1 : 0)}') )); then
+        echo "TRUNC [$fsizeMB] $filePath"
+        truncate -s 0 $filePath
+    fi
+done

caa64d28538c6dcdc655d8ef8368f601f90adbad
qiuqiuqiu <28886115+cronusqiu90@users.noreply.github.com>
2024-10-18 16:59:37 +0800

    Create tmp.sh

---
+++ tmp.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cd /home/auser
+rm -rf upgrade.zip
+rm -rf cd
+rm -rf venv
+rm -rf home
+
+cd /home/auser/TRClient
+rm -rf 5 3 9 7
+rm -rf taskmgr
+rm -rf taskmgr2
+rm -rf agentd
+rm -rf isync
+rm -rf Openlab

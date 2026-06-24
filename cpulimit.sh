3e3e4c8560c5e42a5c59685e4c3e2fe62d53a605
qiuqiuqiu <28886115+cronusqiu90@users.noreply.github.com>
2026-06-24 13:37:25 +0800

    Update cpulimit.sh

--- cpulimit.sh
+++ cpulimit.sh
@@ -4,6 +4,18 @@ LIMIT=70
 pkill -f "cpulimit" 2>/dev/null
 sleep 1
 
+pid=$(pgrep -f "TC6.py")
+if [ -n "$pid" ]; then
+    renice -n 10 -p $pid >/dev/null 2>&1
+    nohup cpulimit -p "$pid" -l $LIMIT -z >/dev/null 2>&1 &
+fi
+
+pid=$(pgrep -f "MQ6.py")
+if [ -n "$pid" ]; then
+    renice -n 10 -p $pid >/dev/null 2>&1
+    nohup cpulimit -p "$pid" -l $LIMIT -z >/dev/null 2>&1 &
+fi
+
 supervisorctl status | grep -E "RUNNING" | while read -r line; do
     program=$(echo "$line" | awk '{print $1}')
     pid=$(echo "$line" | grep -oE 'pid [0-9]+' | awk '{print $2}')
